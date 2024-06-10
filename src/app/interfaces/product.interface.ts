83ac6ab7ab5bdf325f6a7844ba56fe016c36e665
André Silveira <andresilveira.email@gmail.com>
2024-06-09 21:33:58 -0300

    Api em Angular
    Consultando API em Angular

---
+++ src/app/interfaces/product.interface.ts
@@ -0,0 +1,6 @@
+export interface ProductInterface {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  API_URL : string = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<any>{
+    return this.httpClient.get(this.API_URL).pipe(res => res);
+  }
+
+}
